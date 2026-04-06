c3d8a251aa1fd48756dc72285436a22d5d0db803
Greninja9257 <Greninja9257@users.noreply.github.com>
2026-04-05 18:27:38 -0700

    Fix mothership controller for current ioTypes

--- server/Game/gamemodes/mothership.js
+++ server/Game/gamemodes/mothership.js
@@ -53,9 +53,9 @@ class Mothership {
             o.name = "Mothership";
             o.isMothership = true;
             o.controllers.push(
-            new ioTypes.nearestDifferentMaster(o, {}, global.gameManager),
-            new ioTypes.keepDistance(o, { minDistance: o.size * 30, maxDistance: o.size * 54, strafeDistance: o.size * 24 })
-        );
+                new ioTypes.nearestDifferentMaster(o, {}, global.gameManager),
+                new ioTypes.mapTargetToGoal(o)
+            );
             o.refreshBodyAttributes();
             this.motherships.push([o.id, team]);
             this.globalMotherships.push(o);
